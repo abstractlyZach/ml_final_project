27b1e63e56343b6b9a0b4c197bb2ebb9e22ef1bc
exzacktlee <zach3lee@gmail.com>
2016-03-12 17:13:49 -0800

    uploaded Pattiss's example code and wrote my own code in tree.py

---
+++ tree/tree.py
@@ -0,0 +1,68 @@
+class TN:
+	def __init__(self, prediction, parent=None, left=None, right=None, id=None):
+		self.parent = parent
+		self._prediction = prediction
+		self.left  = left
+		self.right = right
+		self._is_leaf = True
+		self.id = id # placed here for debugging purposes
+
+	def size(self):
+		if self.left ==None:
+			left_size = 0
+		else:
+			left_size = self.left.size()
+		if self.right == None:
+			right_size = 0
+		else:
+			right_size = self.right.size()
+		return 1 + right_size + left_size
+
+	def is_leaf(self):
+		return self._is_leaf
+
+	def height(self):
+		return
+
+	def predict(self):
+		return self._prediction
+
+	def print_tree(self, indent_char ='...', indent_delta=3):
+		'''Right branch details the case where the answer to the node's split query (feature x2 > 5, etc.)
+			is true.'''
+		def print_tree_1(indent,atree):
+			if atree.is_leaf():
+				# print('PREDICTING FOR NODE WITH ID: {}'.format(atree.id))
+				# print(atree.is_leaf())
+				print(indent * indent_char, atree.predict())
+			else:
+				print_tree_1(indent+indent_delta,atree.right)
+				print(indent*indent_char+'(X{}>={}?)'.format(atree._feature_to_split, atree._feature_threshold))
+				print_tree_1(indent+indent_delta,atree.left)
+		print_tree_1(0, self) 
+
+	def split(self, feature_to_split, feature_threshold, left_prediction, right_prediction):
+		if not self.is_leaf():
+			raise SplitError
+
+		del self._prediction
+		self._is_leaf = False
+		self._feature_to_split = feature_to_split
+		self._feature_threshold = feature_threshold
+
+		if self.id is not None:
+			self.left = TN(left_prediction, parent = self, id=(self.id * 2) + 1)
+			self.right = TN(right_prediction, parent = self, id=(self.id * 2) + 2)
+		else:
+			self.left = TN(left_prediction, parent = self)
+			self.right = TN(right_prediction, parent = self)
+
+
+
+
+
+
+
+class SplitError(Exception):
+	def __init__(self):
+		print('Tried to split on tree node that was not a leaf.')
---
+++ tree/tree_test.py
@@ -0,0 +1,28 @@
+# testing tree.py
+import tree
+
+decision_tree = tree.TN(3, id=0)
+decision_tree.print_tree()
+print()
+print()
+
+current_node = decision_tree
+current_node.split(2, 5.4, 2, 6)
+
+decision_tree.print_tree()
+print()
+print()
+
+current_node = current_node.left
+current_node.split(99, 37.8, 55, 93)
+
+decision_tree.print_tree()
+print()
+print()
+
+current_node = current_node.parent.right
+current_node.split(77, 334.85, 44, 22)
+
+decision_tree.print_tree()
+print()
+print()
